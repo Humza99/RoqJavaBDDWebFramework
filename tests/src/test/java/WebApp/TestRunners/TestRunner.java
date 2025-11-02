ee483373388bbfb4b665088cebaf861760e8e68b
HumzaMohammed <humzamohammed99@gmail.com>
2025-11-02 13:15:56 +0000

    Setup Multi POM maven project with tests running and passing. Need to cleanup misc files/dependancies + make sure all reporting is sorted

--- tests/src/test/java/WebApp/TestRunners/TestRunner.java
+++ tests/src/test/java/WebApp/TestRunners/TestRunner.java
@@ -1,9 +1,11 @@
+package WebApp.TestRunners;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/Features",
-        glue = {"WebApp.StepDefs", "Framework/Hooks"}, // Add the package names where your step definitions and hooks are located
+        glue = {"WebApp.StepDefs", "Framework.Hooks"}, // Add the package names where your step definitions and hooks are located
         plugin = { "pretty",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
                 "json:target/cucumber-reports/cucumber.json",
