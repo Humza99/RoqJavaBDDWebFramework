47f82d449ef0cc951d140e88a09eeb0b480594ef
HumzaMohammed <humzamohammed99@gmail.com>
2026-03-05 06:34:03 +0000

    pipeline extent report screenshot fix

--- framework/src/main/java/Framework/WebAppDriver.java
+++ framework/src/main/java/Framework/WebAppDriver.java
@@ -3,7 +3,6 @@ package Framework;
 import Utilites.SettingsLoader;
 import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -15,12 +14,7 @@ import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.io.*;
-import java.nio.file.Paths;
 import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class WebAppDriver {
 
@@ -90,34 +84,9 @@ public class WebAppDriver {
     }
 
     public void takeScreenshotOnFailure(Scenario scenarioContext) {
-
-        String tempPath = System.getProperty("java.io.tmpdir") + File.separator + "AutomationFailScreenshots" + File.separator;
-        File directory = new File(tempPath);
-
-        if (!directory.exists()) {
-            try {
-                directory.mkdir();
-            }
-            catch (RuntimeException ex){
-                System.out.print("Failed to create directory");
-            }
-        }
-
-        String fileName = scenarioContext.getName() + LocalDateTime.now().format(DateTimeFormatter.ofPattern(" dd MMMM yyyy HH-mm-ss")) + ".png";
-        String screenshotPath = Paths.get(tempPath, fileName).toString();
-
         if (webDriver != null && scenarioContext.isFailed()) {
-            try {
-                File screenshotFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
-                File targetFile = new File(screenshotPath);
-                FileUtils.copyFile(screenshotFile, targetFile);
-
-                byte[] fileContent = FileUtils.readFileToByteArray(screenshotFile);
-                scenarioContext.attach(fileContent, "image/png", "Test Failed. Local screenshot saved at " + screenshotPath);
-
-            } catch (IOException e) {
-                System.err.println("Failed to take screenshot: " + e.getMessage());
-            }
+            byte [] screenshotFile = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+            scenarioContext.attach(screenshotFile, "image/png", "Test Failed - " + scenarioContext.getName());
         }
     }
 
